285cc041b5bdd1ae5b049c520a2fd46777dfb408
Tara Sherman <taradelin@gmail.com>
2019-12-18 21:51:06 -0500

    Connected files

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 
+const knex = require('knex');
+
+const accountsRouter = require('./accounts/accountsRouter');
+
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
+const logger = require('morgan');
+
 server.use(express.json());
+server.use(logger('dev'));
+server.use('/api/accounts', accountsRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+  <h2>Web DB I Challenge</h2>
+  `)
+});
 
 module.exports = server;
